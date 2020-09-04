2195ad79b729fe775f6bc6dc01a9533b879df6d0
shubhamgarde397 <shubham.garde397@gmail.com>
2020-09-04 15:39:02 +0530

    removed handler and update

--- src/app/pages/LRNumber/lr.module.ts
+++ src/app/pages/LRNumber/lr.module.ts
@@ -3,15 +3,11 @@ import { LRRouting } from './LRRouting';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { LRNumberHandlerComponent } from './lrnumber-handler/lrnumber-handler.component';
 import { LRNumberDisplayComponent } from './lrnumber-display/lrnumber-display.component';
-import { LRNumberUpdateComponent } from './lrnumber-update/lrnumber-update.component';
 
 @NgModule({
     declarations: [
-        LRNumberHandlerComponent,
-        LRNumberDisplayComponent,
-        LRNumberUpdateComponent
+        LRNumberDisplayComponent
     ],
     imports: [
         CommonModule,
