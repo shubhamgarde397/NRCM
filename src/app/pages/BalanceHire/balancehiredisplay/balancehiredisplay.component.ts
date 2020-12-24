b8c37fe0bf5b218e7f6cc8f3fbc428529ef546c5
shubhamgarde397 <shubham.garde397@gmail.com>
2020-12-24 09:48:06 +0530

    PDF New

--- src/app/pages/BalanceHire/balancehiredisplay/balancehiredisplay.component.ts
+++ src/app/pages/BalanceHire/balancehiredisplay/balancehiredisplay.component.ts
@@ -37,6 +37,7 @@ export class BalancehiredisplayComponent implements OnInit {
   public todayDate;
   public balanceDate = [];
   public selectedDate;
+  public document = new jsPDF();
 
   constructor(public apiCallservice: ApiCallsService, public spinnerService: Ng4LoadingSpinnerService, public router: Router,
     public handleData: HandleDataService, public excelService: ExcelService,
@@ -64,69 +65,83 @@ export class BalancehiredisplayComponent implements OnInit {
 
 
 
-  download() {
-    this.spinnerService.show();
-    this.apiCallservice.handleData_New(this.dbName, 'booking/getBookingDetails', 1, 1, {}, this.name)
-      .subscribe((res: Response) => {
-        this.newAuthor = res.json();
-        let rows = [];
-        const columns = [
-          { title: 'Date', dataKey: 'Date' },
-          { title: 'Lrno', dataKey: 'lrno' },
-          { title: 'Truck No', dataKey: 'truckno' },
-          { title: 'Hire Amount', dataKey: 'hireamount' },
-          { title: 'Place', dataKey: 'place' },
-          { title: 'Amount', dataKey: 'amount' },
-          { title: 'Payment', dataKey: 'payment' },
-          { title: 'Payment Date', dataKey: 'paymentdate' }
-        ];
-        const doc = new jsPDF({
-          orientation: 'landscape',
-          unit: 'in',
-          format: [15, 10]
-        });
-        const sender = 'Nitin Roadways And Cargo Movers';
-        const party = this.newAuthor[0].nop;
-        const to = this.newAuthor[0].FromParty;
-        const toGST = this.newAuthor[0].FromPartyGST;
-        const partyGST = this.newAuthor[0].ToPartyGST;
-        doc.setFontSize(40);
-        doc.setFont('Arial');
-        doc.setFontType('bold');
-        doc.setTextColor(255, 0, 0);
-        doc.text(sender, 3, 1.8);
-        doc.setLineWidth(0.1);
-        doc.line(0, 2.2, 15, 2.2);
-        // var doc = new jsPDF('p', 'pt');
-        doc.setFontSize(60);
-        doc.setTextColor(0, 0, 0);
-        doc.text(party, 4, 3.5);
-        doc.setLineWidth(0.1);
-        doc.line(0, 4.2, 15, 4.2);
-        doc.setFontSize(16);
-        doc.text(name + ' : ' + partyGST, 8, 4.6);
-        doc.text(to + ' : ' + toGST, 8, 5);
+  download() {//threshhold is 295
 
-        doc.addPage();
-        let i = 1;
-        // tslint:disable-next-line:forin
-        for (const key in this.newAuthor) {
-          rows = [...rows, Object.assign({}, {
-            'Date': this.newAuthor[key].Date,
-            'lrno': this.newAuthor[key].lrno,
-            'truckno': this.newAuthor[key].truckno,
-            'hireamount': this.newAuthor[key].hamt,
-            'place': this.newAuthor[key].place,
-            'amount': this.newAuthor[key].amt,
-            'payment': this.newAuthor[key].Payment,
-            'paymentdate': this.newAuthor[key].PaymentReCDate,
-          })];
-          i++;
-        }
-        doc.autoTable(columns, rows);
-        doc.save('' + name + '.pdf');
-        this.spinnerService.hide();
-      });
+    var doc = this.document;
+    //Static Part Start
+    //Date
+    doc.setFontSize('15');
+    doc.setFontType('bold');
+    doc.setTextColor(0, 0, 0);
+    doc.text(this.balanceDate[0].todayDate, 90, 5)
+    //Date
+    //line after date
+    doc.setDrawColor(0, 0, 0);
+    doc.setLineWidth(0.5);
+    doc.line(0, 6, 210, 6);
+    //line after date
+    //5 vertical lines for amount, comments, pageno,date,trucno, account details(account details is further divided into 3 parts per data need a loop here)
+    //vertical line after date
+    doc.setDrawColor(0, 0, 0);
+    doc.setLineWidth(0.5);
+    doc.line(22, 6, 22, 300);
+    doc.line(46, 6, 46, 300);
+    doc.line(57, 6, 55.4, 300);
+    doc.line(72, 6, 72, 300);
+    doc.line(120, 6, 120, 300);
+    //vertical line after date
+    //Headers
+    doc.setFontSize('10');
+    doc.setFontType('bold');
+    doc.setTextColor(0, 0, 0);
+    doc.text('Amount', 1, 10)
+    doc.text('Comments', 23, 10)
+    doc.text('Pg', 48, 10)
+    doc.text('Date', 57.5, 10)
+    doc.text('TruckNo', 72.5, 10)
+    doc.text('Account Details', 120.5, 10)
+    //Headers End
+    //Line after headers
+    doc.setDrawColor(0, 0, 0);
+    doc.setLineWidth(0.5);
+    doc.line(0, 12, 210, 12);
+    //Line after headers
+    //Static Part End
+    //Dynamic Part Start
+    doc.setFontSize('20');
+    doc.setFontType('normal');
+    doc.setTextColor(0, 0, 0);
+    let i = 18;
+    // doc.text('Shubham is awesome', 1, i);
+    for (let z = 0; z < this.balanceDate.length; z++) {
+      let data = this.balanceDate[z].truckData;
+
+      let K = 0
+      for (let k = 0; k < data.length; k++) {
+        doc.setFontSize('20');
+        doc.text(String(this.balanceDate[z].truckData[k].amount), 1, i);//amount
+        // doc.text(this.balanceDate[z].truckData[k].amount,i,10);//comments
+        doc.setFontSize('15');
+        doc.text(String(this.balanceDate[z].truckData[k].pageno), 46.5, i);//pgno
+        doc.text(this.balanceDate[z].truckData[k].date.slice(8, 10) + '/' + this.balanceDate[z].truckData[k].date.slice(5, 7), 57.5, i);//date
+        doc.setFontSize('20');
+        doc.text(this.balanceDate[z].truckData[k].truckno, 72.5, i);//truckno
+        K = k;
+        i = i + 6;
+      }
+      doc.line(0, i + 7, 210, i + 1);
+      doc.setFontSize('15');
+      doc.text(this.balanceDate[z].accountNumber, 121.5, i - (data.length * 6));//accno
+      doc.text(this.balanceDate[z].ifsc + '-' + this.balanceDate[z].bankname, 121.5, i + 6 - (data.length * 6));//ifsc-bankname
+      doc.text(this.balanceDate[z].accountName, 121.5, i + 12 - (data.length * 6));//accname
+      i = i + 10;
+
+
+    }
+    //Dynamic Part End
+    console.log(i);
+
+    doc.save('tp.pdf')
   }
 }
 
--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { timeout } from 'q';
 
+import * as jsPDF from 'jspdf';
+import 'jspdf-autotable';
 import { ApiCallsService } from '../../common/services/ApiCalls/ApiCalls.service';
 import { HandleDataService } from '../../common/services/Data/handle-data.service';
 import { handleFunction } from '../../common/services/functions/handleFunctions';
@@ -13,6 +14,322 @@ import { handleFunction } from '../../common/services/functions/handleFunctions'
   providers: [ApiCallsService]
 })
 export class MainPageComponent implements OnInit {
+  temp = [
+    {
+      "_id": "5fd05b4139d88ed9d85df96c",
+      "truckData": [
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 2950
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 2950
+        }
+      ],
+      "todayDate": "2020-12-09",
+      "bankname": "ICICI",
+      "ifsc": "ICIC0",
+      "accountNumber": "3265",
+      "accountName": "Shubham"
+    },
+
+    {
+      "_id": "5fdef546d778c0bc817ffa9c",
+      "truckData": [
+        {
+          "date": "2020-12-09",
+          "truckno": "AP03 TC 6831",
+          "pageno": 23,
+          "amount": 123
+        },
+        {
+          "date": "2020-12-21",
+          "truckno": "AP03 TC 5589",
+          "pageno": 23,
+          "amount": 3456
+        },
+        {
+          "date": "2020-12-09",
+          "truckno": "AP03 TC 6831",
+          "pageno": 23,
+          "amount": 123
+        }, {
+          "date": "2020-12-09",
+          "truckno": "AP03 TC 6831",
+          "pageno": 23,
+          "amount": 123
+        }, {
+          "date": "2020-12-09",
+          "truckno": "AP03 TC 6831",
+          "pageno": 23,
+          "amount": 123
+        }, {
+          "date": "2020-12-09",
+          "truckno": "AP03 TC 6831",
+          "pageno": 23,
+          "amount": 123
+        }
+      ],
+      "todayDate": "2020-12-09",
+      "bankname": "Canara",
+      "ifsc": "CNRB",
+      "accountNumber": "6258",
+      "accountName": "Nitin"
+    },
+    {
+      "_id": "5fd05b4139d88ed9d85df96c",
+      "truckData": [
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 2950
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 2950
+        }
+      ],
+      "todayDate": "2020-12-09",
+      "bankname": "ICICI",
+      "ifsc": "ICIC0",
+      "accountNumber": "3265",
+      "accountName": "Shubham"
+    },
+    {
+      "_id": "5fd05b4139d88ed9d85df96c",
+      "truckData": [
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 2950
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 2950
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 2950
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 2950
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 2950
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 2950
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 29500
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 29500
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 2950
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 2950
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 2950
+        }
+      ],
+      "todayDate": "2020-12-09",
+      "bankname": "ICICI",
+      "ifsc": "ICIC0",
+      "accountNumber": "3265",
+      "accountName": "Shubham"
+    }, {
+      "_id": "5fd05b4139d88ed9d85df96c",
+      "truckData": [
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 2950
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 2950
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 2950
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 2950
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 2950
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 2950
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 29500
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 29500
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 2950
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 2950
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 2950
+        }
+      ],
+      "todayDate": "2020-12-09",
+      "bankname": "ICICI",
+      "ifsc": "ICIC0",
+      "accountNumber": "3265",
+      "accountName": "Shubham"
+    }, {
+      "_id": "5fd05b4139d88ed9d85df96c",
+      "truckData": [
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 2950
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 2950
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 2950
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 2950
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 2950
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 2950
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 29500
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 29500
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 2950
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 2950
+        },
+        {
+          "date": "2020-12-08",
+          "truckno": "TN32 M 2542",
+          "pageno": 78,
+          "amount": 2950
+        }
+      ],
+      "todayDate": "2020-12-09",
+      "bankname": "ICICI",
+      "ifsc": "ICIC0",
+      "accountNumber": "3265",
+      "accountName": "Shubham"
+    }]
+
 
   modalData;
   public loginV = false;
@@ -24,7 +341,7 @@ export class MainPageComponent implements OnInit {
   public clue = false;
   public fileFormData = new FormData();
   public fileFormDataPython = new FormData();
-
+  public newAuthor: any;
   // 
   selectedFile = null;
   imageFolder = ''
@@ -34,10 +351,89 @@ export class MainPageComponent implements OnInit {
   //json
   finalJson = {};
   public pythonVar = '';
+  public document = new jsPDF();
   // 
   constructor(public apiCallservice: ApiCallsService, public handledata: HandleDataService,
     public router: Router, public handlefunction: handleFunction) { }
+  pdf() {//threshhold is 295
 
+    var doc = this.document;
+    //Static Part Start
+    //Date
+    doc.setFontSize('15');
+    doc.setFontType('bold');
+    doc.setTextColor(0, 0, 0);
+    doc.text(this.temp[0].todayDate, 90, 5)
+    //Date
+    //line after date
+    doc.setDrawColor(0, 0, 0);
+    doc.setLineWidth(0.5);
+    doc.line(0, 6, 210, 6);
+    //line after date
+    //5 vertical lines for amount, comments, pageno,date,trucno, account details(account details is further divided into 3 parts per data need a loop here)
+    //vertical line after date
+    doc.setDrawColor(0, 0, 0);
+    doc.setLineWidth(0.5);
+    doc.line(22, 6, 22, 300);
+    doc.line(47, 6, 47, 300);
+    doc.line(57, 6, 55.4, 300);
+    doc.line(72, 6, 72, 300);
+    doc.line(120, 6, 120, 300);
+    //vertical line after date
+    //Headers
+    doc.setFontSize('10');
+    doc.setFontType('bold');
+    doc.setTextColor(0, 0, 0);
+    doc.text('Amount', 1, 10)
+    doc.text('Comments', 23, 10)
+    doc.text('Pg', 48, 10)
+    doc.text('Date', 57.5, 10)
+    doc.text('TruckNo', 72.5, 10)
+    doc.text('Account Details', 120.5, 10)
+    //Headers End
+    //Line after headers
+    doc.setDrawColor(0, 0, 0);
+    doc.setLineWidth(0.5);
+    doc.line(0, 12, 210, 12);
+    //Line after headers
+    //Static Part End
+    //Dynamic Part Start
+    doc.setFontSize('20');
+    doc.setFontType('normal');
+    doc.setTextColor(0, 0, 0);
+    let i = 18;
+    // doc.text('Shubham is awesome', 1, i);
+    for (let z = 0; z < this.temp.length; z++) {
+      let data = this.temp[z].truckData;
+      if (data.length == 1) {
+
+      }
+      if (data.length > 1) {
+        let K = 0
+        for (let k = 0; k < data.length; k++) {
+          doc.text(String(this.temp[z].truckData[k].amount), 1, i);//amount
+          // doc.text(this.temp[z].truckData[k].amount,i,10);//comments
+          doc.setFontSize('15');
+          doc.text(String(this.temp[z].truckData[k].pageno), 48, i);//pgno
+          doc.text(this.temp[z].truckData[k].date.slice(8, 10) + '/' + this.temp[z].truckData[k].date.slice(5, 7), 57.5, i);//date
+          doc.setFontSize('20');
+          doc.text(this.temp[z].truckData[k].truckno, 72.5, i);//truckno
+          K = k;
+          i = i + 6;
+        }
+        doc.line(0, i + 1, 210, i + 1);
+        doc.text(this.temp[z].accountNumber, 121.5, i - (data.length * 6));//accno
+        doc.text(this.temp[z].ifsc + '-' + this.temp[z].bankname, 121.5, i + 6 - (data.length * 6));//ifsc-bankname
+        doc.text(this.temp[z].accountName, 121.5, i + 12 - (data.length * 6));//accname
+        i = i + 7;
+      }
+
+    }
+    //Dynamic Part End
+    console.log(i);
+
+    doc.save('tp.pdf')
+  }
 
   login() {
     this.router.navigate(['Login']);
@@ -71,6 +467,7 @@ export class MainPageComponent implements OnInit {
       })
   }
   ngOnInit() {
+    console.log(this.temp);
 
     setInterval(() => {
       this.now = new Date();
