685a6edee12303a0b010fc67eb5de77b96cc1775
shubhamgarde397 <shubham.garde397@gmail.com>
2020-12-24 11:16:05 +0530

    Newpdf2

--- src/app/pages/BalanceHire/balancehiredisplay/balancehiredisplay.component.ts
+++ src/app/pages/BalanceHire/balancehiredisplay/balancehiredisplay.component.ts
@@ -112,9 +112,58 @@ export class BalancehiredisplayComponent implements OnInit {
     doc.setFontType('normal');
     doc.setTextColor(0, 0, 0);
     let i = 18;
+    console.log('start : ', i);
+
     // doc.text('Shubham is awesome', 1, i);
     for (let z = 0; z < this.balanceDate.length; z++) {
       let data = this.balanceDate[z].truckData;
+      if (((data.length * 6) + 15 + i) > 295) {
+        doc.addPage();
+        //Static Part Start
+        //Date
+        doc.setFontSize('15');
+        doc.setFontType('bold');
+        doc.setTextColor(0, 0, 0);
+        doc.text(this.balanceDate[0].todayDate, 90, 5)
+        //Date
+        //line after date
+        doc.setDrawColor(0, 0, 0);
+        doc.setLineWidth(0.5);
+        doc.line(0, 6, 210, 6);
+        //line after date
+        //5 vertical lines for amount, comments, pageno,date,trucno, account details(account details is further divided into 3 parts per data need a loop here)
+        //vertical line after date
+        doc.setDrawColor(0, 0, 0);
+        doc.setLineWidth(0.5);
+        doc.line(22, 6, 22, 300);
+        doc.line(46, 6, 46, 300);
+        doc.line(57, 6, 55.4, 300);
+        doc.line(72, 6, 72, 300);
+        doc.line(120, 6, 120, 300);
+        //vertical line after date
+        //Headers
+        doc.setFontSize('10');
+        doc.setFontType('bold');
+        doc.setTextColor(0, 0, 0);
+        doc.text('Amount', 1, 10)
+        doc.text('Comments', 23, 10)
+        doc.text('Pg', 48, 10)
+        doc.text('Date', 57.5, 10)
+        doc.text('TruckNo', 72.5, 10)
+        doc.text('Account Details', 120.5, 10)
+        //Headers End
+        //Line after headers
+        doc.setDrawColor(0, 0, 0);
+        doc.setLineWidth(0.5);
+        doc.line(0, 12, 210, 12);
+        //Line after headers
+        //Static Part End
+        //Dynamic Part Start
+        doc.setFontSize('20');
+        doc.setFontType('normal');
+        doc.setTextColor(0, 0, 0);
+        i = 18;
+      }
 
       let K = 0
       for (let k = 0; k < data.length; k++) {
@@ -129,17 +178,18 @@ export class BalancehiredisplayComponent implements OnInit {
         K = k;
         i = i + 6;
       }
-      doc.line(0, i + 7, 210, i + 1);
+      doc.line(0, i + 7, 210, i + 7);
       doc.setFontSize('15');
       doc.text(this.balanceDate[z].accountNumber, 121.5, i - (data.length * 6));//accno
       doc.text(this.balanceDate[z].ifsc + '-' + this.balanceDate[z].bankname, 121.5, i + 6 - (data.length * 6));//ifsc-bankname
       doc.text(this.balanceDate[z].accountName, 121.5, i + 12 - (data.length * 6));//accname
-      i = i + 10;
+      i = i + 15;
+      console.log('val of i : ', i);
 
 
     }
     //Dynamic Part End
-    console.log(i);
+    console.log('end i : ', i);
 
     doc.save('tp.pdf')
   }
