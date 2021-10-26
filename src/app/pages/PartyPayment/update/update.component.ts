b8f3691c68411f306305cb69d57789ede09282ed
shubhamgarde397 <shubham.garde397@gmail.com>
2021-10-26 14:21:02 +0530

    Splice bugs

--- src/app/pages/PartyPayment/update/update.component.ts
+++ src/app/pages/PartyPayment/update/update.component.ts
@@ -16,12 +16,12 @@ export class UpdateComponent implements OnInit {
   public pendingTrucks;
   public addToArrayVar=[]
   public pp;
+  public index;
   constructor(public hd:HandleDataService,public apiCallservice:ApiCallsService,public router:Router,public hf:handleFunction) { }
 
   ngOnInit() {
     this.loadingDate=this.hd.givePPData()[0]['loadingDate'];
     this.allData=this.hd.givePPData();
-    console.log(this.allData);
     
   }
 
@@ -32,7 +32,6 @@ export class UpdateComponent implements OnInit {
 
   getAllTBPendingTrucks(){
     let tempobj={}
-    console.log(this.allData);
     
       tempobj['partyid']=this.allData[0]['partyid'];
       tempobj["method"]= "pendingPayment",
@@ -42,7 +41,6 @@ export class UpdateComponent implements OnInit {
       this.apiCallservice.handleData_New_python('commoninformation', 1, tempobj, 0)
       .subscribe((response: Response) => {
         this.pendingTrucks=response['Data'];
-        console.log(this.pendingTrucks);
         
       });
 
@@ -50,31 +48,42 @@ export class UpdateComponent implements OnInit {
 
   addToArray(){
     let tempObj={}
-    console.log(this.pp);
-    
-    tempObj['truckno']=this.pendingTrucks[this.hf.findpayment(this.pp)]['data'].split('_')[2]
-    tempObj['date']=this.pendingTrucks[this.hf.findpayment(this.pp)]['data'].split('_')[0]
-    tempObj['lrno']=this.pendingTrucks[this.hf.findpayment(this.pp)]['lrno']
-    tempObj['id']=this.pendingTrucks[this.hf.findpayment(this.pp)]['_id']
+    this.index=parseInt(this.hf.findpayment(this.pp));
+    tempObj['truckno']=this.pendingTrucks[this.index]['data'].split('_')[2]
+    tempObj['date']=this.pendingTrucks[this.index]['data'].split('_')[0]
+    tempObj['lrno']=this.pendingTrucks[this.index]['lrno']
+    tempObj['id']=this.pendingTrucks[this.index]['_id']
+    tempObj['partyName']=this.pendingTrucks[this.index]['partyName']
+    tempObj['entryDate']=this.pendingTrucks[this.index]['data'].split('_')[1]
+    tempObj['index']=this.index;
+    this.pendingTrucks.splice(this.index,1);
     this.addToArrayVar.push(tempObj)
   }
 
   addToSave(){
-    console.log(this.addToArrayVar);
     let tempobj={}
     tempobj['tbids']=this.addToArrayVar.map(r=>r.id);
     tempobj['id']=this.allData[0]['_id'];
     tempobj['method']='addIdsToPartyAndTB';
     tempobj['tablename']='partyPayment';
-    this.apiCallservice.handleData_New_python('commoninformation', 1, tempobj, 0)
+    this.apiCallservice.handleData_New_python('commoninformation1', 1, tempobj, 0)
         .subscribe((response: Response) => {
           alert(response['Status']);
         });
     
   }
 
-  tempDelete(j){
-    this.addToArrayVar.splice(j,1)
+  tempDelete(j,i){
+    this.addToArrayVar.splice(j,1);
+let tempObj={}
+tempObj['date']=i['date']
+tempObj['lrno']=i['lrno']
+tempObj['partyName']=i['partyName']
+tempObj['_id']=i['id']
+tempObj['data']=i['date']+'_'+i['entryDate']+'_'+i['truckno']+'_'+i['id']
+    this.pendingTrucks.splice(i['index'],0,tempObj);
+    
+    
   }
   
   delete(index){
