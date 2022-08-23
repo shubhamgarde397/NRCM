f713c13ca6ac9ae286ca5a1f92b60957a7ab95df
shubhamgarde397 <shubham.garde397@gmail.com>
2022-08-23 19:42:27 +0530

    t8

--- src/app/pages/TurnBook/Display/turn-book-display-main/turn-book-display-main.component.ts
+++ src/app/pages/TurnBook/Display/turn-book-display-main/turn-book-display-main.component.ts
@@ -998,16 +998,16 @@ this.placeid=this.tempDate[0]['place']['_id']
      doc.text('Sr', 2, y)//partyname
      doc.text('TruckNo', 8, y)//partyname
      doc.text('Personal Details', 36, y)//partyname
-     doc.text('Account Details', 99, y)//partyname
+     doc.text('Account Details', 105, y)//partyname
      doc.text('Docs', 166, y)//partyname
-     doc.text('Policy', 188, y)//partyname
+     doc.text('Registration', 188, y)//partyname
      //headers
      doc.line(0, 25, 210, 25);//line after header
  
      //vertical lines
      doc.line(7, 20, 7, 25);//srno
      doc.line(33, 20, 33, 25);//date
-     doc.line(97, 20, 97, 25);//date
+     doc.line(103, 20, 103, 25);//date
     //  doc.line(161, 20, 161, 25);//date
      doc.line(165, 20, 165, 25);//date
     //  doc.line(175, 20, 175, 25);//date
@@ -1021,7 +1021,7 @@ this.placeid=this.tempDate[0]['place']['_id']
        if(y>276){
         doc.line(7, starty, 7, y-4);//srno
         doc.line(33, starty, 33, y-4);//date 
-        doc.line(97, starty, 97, y-4);//date
+        doc.line(103, starty, 103, y-4);//date
         doc.line(161, starty, 161, y-4);//date
         doc.line(165, starty, 165, y-4);//date
         doc.line(175, starty, 175, y-4);//date
@@ -1045,16 +1045,16 @@ this.placeid=this.tempDate[0]['place']['_id']
      doc.text('Sr', 2, y-6)//partyname
      doc.text('TruckNo', 8, y-6)//partyname
      doc.text('Personal Details', 36, y-6)//partyname
-     doc.text('Account Details', 99, y-6)//partyname
+     doc.text('Account Details', 105, y-6)//partyname
      doc.text('Docs', 166, y-6)//partyname
-     doc.text('Policy', 188, y-6)//partyname
+     doc.text('Registration', 188, y-6)//partyname
      //headers
      doc.line(0, 25, 210, 25);//line after header
  
      //vertical lines
      doc.line(7, 20, 7, 25);//srno
      doc.line(33, 20, 33, 25);//date
-     doc.line(97, 20, 97, 25);//date
+     doc.line(103, 20, 103, 25);//date
      doc.line(161, 20, 161, 25);//date
      doc.line(165, 20, 165, 25);//date
     //  doc.line(175, 20, 175, 25);//date
@@ -1065,10 +1065,10 @@ this.placeid=this.tempDate[0]['place']['_id']
       doc.text(data[i].ownerDetails[0].truckno.split(' ')[0]+''+data[i].ownerDetails[0].truckno.split(' ')[1]+''+data[i].ownerDetails[0].truckno.split(' ')[2], 8, y-1)//partyname
       doc.text(data[i]['ownerDetails'][0]['pan']===""?'Name : ':'Name : '+data[i]['ownerDetails'][0]['oname'], 34, y)//Name
       doc.text(data[i]['ownerDetails'][0]['pan']===""?'Pan : ':'Pan : '+data[i]['ownerDetails'][0]['pan'], 34, y+5)//Pan
-      doc.text(data[i]['ownerDetails'][0]['contact'][0]===undefined?'Contact : ':'Contact : '+data[i]['ownerDetails'][0]['contact'][0], 34, y+10)//Contact
-      doc.text(data[i]['ownerDetails'][0]['accountDetails'].length<1?'Name : ':'Name : '+data[i]['ownerDetails'][0]['accountDetails'][0]['accountName'], 99, y)//Name
-      doc.text(data[i]['ownerDetails'][0]['accountDetails'].length<1?'No : ':'No : '+data[i]['ownerDetails'][0]['accountDetails'][0]['accountNumber'], 99, y+5)//Pan
-      doc.text(data[i]['ownerDetails'][0]['accountDetails'].length<1?'IFSC : ':'IFSC : '+data[i]['ownerDetails'][0]['accountDetails'][0]['ifsc'], 99, y+10)//Contact
+      doc.text(data[i]['ownerDetails'][0]['contact'][0]===undefined?'Contact : ':'Contact : '+data[i]['ownerDetails'][0]['contact'][0]+(data[i]['ownerDetails'][0]['contactO'][0]===undefined?'':','+data[i]['ownerDetails'][0]['contactO'][0]), 34, y+10)//Contact
+      doc.text(data[i]['ownerDetails'][0]['accountDetails'].length<1?'Name : ':'Name : '+data[i]['ownerDetails'][0]['accountDetails'][0]['accountName'], 105, y)//Name
+      doc.text(data[i]['ownerDetails'][0]['accountDetails'].length<1?'No : ':'No : '+data[i]['ownerDetails'][0]['accountDetails'][0]['accountNumber'], 105, y+5)//Pan
+      doc.text(data[i]['ownerDetails'][0]['accountDetails'].length<1?'IFSC : ':'IFSC : '+data[i]['ownerDetails'][0]['accountDetails'][0]['ifsc'], 105, y+10)//Contact
 
       doc.text('RC',167,y)
       doc.text('Pan',167,y+5)
@@ -1078,7 +1078,7 @@ this.placeid=this.tempDate[0]['place']['_id']
       doc.text(data[i]['ownerDetails'][0].P?'Ok':'',177,y+5)
       doc.text(data[i]['ownerDetails'][0].d?'Ok':'',177,y+10)
 
-      doc.text(data[i]['ownerDetails'][0].p?'Ok':'',188,y)
+      doc.text(data[i]['ownerDetails'][0].regFee?'Ok':'',188,y)
 
 
 
@@ -1092,7 +1092,7 @@ this.placeid=this.tempDate[0]['place']['_id']
 //vertical lines//getting applied for every loop, make it happen once only
 doc.line(7, starty, 7, y-4);//srno
         doc.line(33, starty, 33, y-4);//date 
-        doc.line(97, starty, 97, y-4);//date
+        doc.line(103, starty, 103, y-4);//date
         doc.line(165, starty, 165, y-4);//date
         doc.line(175, starty, 175, y-4);//date
         doc.line(187, starty, 187, y-4);//date
